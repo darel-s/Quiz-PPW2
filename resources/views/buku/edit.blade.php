67744276b7262dffa448b66bc641aa1bab523db8
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-11-10 15:57:20 +0700

    add button thumbnail

--- resources/views/buku/edit.blade.php
+++ resources/views/buku/edit.blade.php
@@ -7,7 +7,7 @@
     <body class="bg-gray-100">
     <div class="container mx-auto mt-10">
         <h2 class="text-2xl font-semibold mb-4">Edit Buku</h2>
-        <form action="{{ route('buku.update', $buku->id) }}" method="POST">
+        <form action="{{ route('buku.update', $buku->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="mb-4">
                 <label for="judul" class="block text-gray-700 font-bold">Judul</label>
@@ -28,6 +28,37 @@
             <div>
                 <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Simpan</button>
                 <a href="/buku" class="text-gray-600 ml-2">Batal</a>
+            </div>
+            <div class="col-span-full mt-6">
+                <label for="thumbnail" class="block text-sm font-medium leading-6 text-gray-900">Thumbnail</label>
+                <div class="mt-2">
+                    <input type="file" name="thumbnail" id="thumbnail" >
+                </div>
+            </div>
+            <div class="col-span-full mt-6">
+                <label for="gallery" class="block text-sm font-medium leading-6 text-gray-900">Gallery</label>
+                <div class="mt-2" id="fileinput_wrapper">
+
+                </div>
+                <a href="javascript:void(0);" id="tambah" onclick="addFileInput()">Tambah</a>
+                <script type="text/javascript">
+                    function addFileInput () {
+                        var div = document.getElementById('fileinput_wrapper');
+                        div.innerHTML += '<input type="file" name="gallery[]" id="gallery" class="block w-full mb-5" style="margin-bottom:5px;">';
+                    };
+                </script>
+            </div>
+            <div class="gallery_items">
+                @foreach($buku->galleries()->get() as $gallery)
+                    <div class="gallery_item">
+                        <img
+                        class="rounded-full object-cover object-center"
+                        src="{{ asset($gallery->path) }}"
+                        alt=""
+                        width="400"
+                        />
+                    </div>
+                @endforeach
             </div>
         </form>
     </div>
